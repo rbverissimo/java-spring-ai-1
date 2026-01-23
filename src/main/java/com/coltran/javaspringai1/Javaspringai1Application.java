305dcb7b622035b2fd05c52642b07b4c079cec77
Renato Verissimo <rverissimo.dev@gmail.com>
2026-01-23 16:00:05 -0300

    feat: initial commit

---
+++ src/main/java/com/coltran/javaspringai1/Javaspringai1Application.java
@@ -0,0 +1,13 @@
+package com.coltran.javaspringai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaspringai1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaspringai1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coltran/javaspringai1/Javaspringai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coltran.javaspringai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaspringai1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
